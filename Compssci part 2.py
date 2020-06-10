89724d711fe74239afb34b27d9c56ffa7af46df6
joeulam <56980755+joeulam@users.noreply.github.com>
2020-06-10 14:22:38 -0400

    Step 1

---
+++ Compssci part 2.py
@@ -0,0 +1,11 @@
+def assignment1():
+  print("Assignment 1 completed")
+  
+assignment1()
+
+def assignment2():
+  print("Assignment 2 completed")
+
+assignment2_result = assignment2()
+
+print(assignment2_result)
